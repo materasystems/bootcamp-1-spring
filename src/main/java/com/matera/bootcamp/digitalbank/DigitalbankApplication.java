fae1baa8e362bf77cdd202024b74d08ec0af67ed
vagrant <vagrant@vagrant.vm>
2021-01-19 23:09:54 -0200

    Iniciando projeto Spring Boot

---
+++ src/main/java/com/matera/bootcamp/digitalbank/DigitalbankApplication.java
@@ -0,0 +1,14 @@
+package com.matera.bootcamp.digitalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/matera/bootcamp/digitalbank/DigitalbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.bootcamp.digitalbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
